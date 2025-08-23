d291970449a3f411e87185e0c1613768c685ecf1
Bulat <okhotnikov_99@bk.ru>
2025-08-23 13:22:16 +0300

    add auth provider right

--- src/main/java/com/UserPassportBoot/config/SecurityConfig.java
+++ src/main/java/com/UserPassportBoot/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .authenticationProvider(authProvider)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/first/**", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/users/**", "/passports/**").authenticated()
